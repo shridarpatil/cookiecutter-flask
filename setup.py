d29554315dc20677ddfbe791d96ceca31a7a40ee
shridhar <shridhar.p@zerodha.com>
2017-12-03 03:42:00 +0530

    Tox
    Py test

---
+++ setup.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""Setup.py"""
+from setuptools import setup
+
+requirements = ['pymysql', 'flask']
+
+tests_requirements = [
+    'pytest',
+    'pytest-cov',
+    'pytest-profiling',
+]
+
+
+setup(
+    name='bitcoin_alert',
+    version='0.1',
+    description="",
+    author="Shridhar, Sumeet",
+    author_email='shridharpatil2792@gmai.com',
+    url='https://bitbucket.org/_shridharpatil/bitcoin_alert',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Plugins',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Office/Business',
+    ],
+    license='BSD',
+    install_requires=requirements,
+    test_suite='tests',
+    tests_require=tests_requirements,
+)
---
+++ tests/__init__.py
@@ -0,0 +1,7 @@
+# !/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+    tests/__init__.py
+
+    :license: see LICENSE for more details.
+"""
