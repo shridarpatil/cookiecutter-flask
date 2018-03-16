25c1553d8323617accb3a8d5a44a36b96e5d2d8c
Shridhar Patil <shridharpatil2792@gmail.com>
2018-03-16 17:47:25 +0530

    Project name for main folder

--- {{cookiecutter.project}}/setup.py
+++ {{cookiecutter.project}}/setup.py
@@ -13,12 +13,12 @@ tests_requirements = [
 
 
 setup(
-    name={{cookiecutter.package}},
+    name='{{cookiecutter.package}}',
     version='0.1',
     description="",
-    author="Shridhar, Sumeet",
-    author_email='shridharpatil2792@gmai.com',
-    url='https://bitbucket.org/_shridharpatil/{{cookiecutter.package}}',
+    author="{{cookiecutter.author}}",
+    author_email='{{cookiecutter.email}}',
+    url='{{cookiecutter.url}}',
     classifiers=[
         'Development Status :: 4 - Beta',
         'Environment :: Plugins',
@@ -29,7 +29,7 @@ setup(
         'Programming Language :: Python',
         'Topic :: Office/Business',
     ],
-    license='BSD',
+    license='{{cookiecutter.license}}',
     install_requires=requirements,
     test_suite='tests',
     tests_require=tests_requirements,
